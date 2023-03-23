2ff7bb46b125b9e6bc48af6a83532daab8bbc59d
Gean Martins <36777701+GeanMartinsSilva@users.noreply.github.com>
2023-03-23 10:51:50 -0300

    att

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c76ea184ed4fc6649aa110af219be684",
-"/": "c76ea184ed4fc6649aa110af219be684",
-"main.dart.js": "b4f3a1db544c4733e4f46f040a7780ff",
+"index.html": "de5cd713ab747205fe5ec1c2a0455b96",
+"/": "de5cd713ab747205fe5ec1c2a0455b96",
+"main.dart.js": "3ea382af6ea5617bf9b04f6c2c4a3f61",
 "manifest.json": "0c2782083b55b1a62b0b3f1f1891618c",
 "version.json": "07908173b54a4228bc98869dfbd8376b"
 };
